80632756abe7e1d70d3029d21d326bd596f4ffa1
loujeen <loujeenhazem2003@gmail.cpm>
2024-07-21 17:32:59 +0300

    add Local

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      locale: Locale("en"),
       initialRoute:HomeScreen.routeName ,
       routes: {HomeScreen.routeName: (context) => HomeScreen()},
     );
